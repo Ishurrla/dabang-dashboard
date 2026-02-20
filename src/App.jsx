6e60ee229f6c148d21c47361b43189a69302d7a4
Abdulrahman Abdulrahman <ishurrlaabdulrahman@gmail.com>
2026-02-20 12:56:13 +0100

    tablet screen fix

--- src/App.jsx
+++ src/App.jsx
@@ -23,18 +23,18 @@ export default function App() {
 
       {/* Row 2: Total Revenue + Customer Satisfaction + Target vs Reality */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-7 gap-8 mb-4 items-stretch">
-        <div className="lg:col-span-3 flex flex-col"><TotalRevenue /></div>
+        <div className="md:col-span-2 lg:col-span-3 flex flex-col"><TotalRevenue /></div>
         <div className="lg:col-span-2 flex flex-col"><CustomerSatisfaction /></div>
-        <div className="md:col-span-2 lg:col-span-2 flex flex-col">
+        <div className="lg:col-span-2 flex flex-col">
           <TargetVsReality />
         </div>
       </div>
 
       {/* Row 3: Top Products + Sales Mapping + Volume vs Service */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-7 gap-7">
-        <div className="lg:col-span-3"><TopProducts /></div>
+        <div className="md:col-span-2 lg:col-span-3"><TopProducts /></div>
         <div className="lg:col-span-2 h-full"><SalesMapping /></div>
-        <div className="md:col-span-2 lg:col-span-2 h-full">
+        <div className="lg:col-span-2 h-full">
           <VolumeVsService />
         </div>
       </div>
